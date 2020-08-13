4a6c83ad534658ddab4ec2593ff28aabec0834fd
Vladyslav Zaiets <rutova2@gmail.com>
2020-08-13 12:05:12 +0300

    Small refactor - PwaService
    no behaviour change intended.

---
+++ Services/PwaService.cs
@@ -0,0 +1,50 @@
+using AspNetSpaTemplate.DTOs;
+using AspNetSpaTemplate.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetSpaTemplate.Services;
+
+public class PwaService : IPwaService
+{
+    public Task<PwaStatusResponse> GetStatusAsync(int userId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PushSubscription> RegisterSubscriptionAsync(int userId, RegisterSubscriptionRequest request, string? userAgent, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UnsubscribeAsync(int userId, string endpoint, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PushDeliveryResult> SendPushToUserAsync(int userId, PushNotificationPayload payload, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BatchPushDeliveryResult> BroadcastPushAsync(IReadOnlyList<int> userIds, PushNotificationPayload payload, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SyncQueueEntryResponse> QueueSyncEntryAsync(int userId, SyncQueueEntryRequest request, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<SyncQueueEntryResponse>> GetPendingSyncEntriesAsync(int userId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SyncReplayResult> ReplaySyncQueueAsync(int userId, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+}
