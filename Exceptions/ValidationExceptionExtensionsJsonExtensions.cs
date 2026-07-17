18c76c74e005dffcfc81df7aaf2edf32aa9f53dc
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    tighten guard clauses in ValidationExceptionExtensionsJsonExtensions.

--- Exceptions/ValidationExceptionExtensionsJsonExtensions.cs
+++ Exceptions/ValidationExceptionExtensionsJsonExtensions.cs
@@ -63,8 +63,11 @@ public static class ValidationExceptionExtensionsJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized exception if successful.</param>
     /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
     public static bool TryFromJson(string json, out ValidationException? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         value = null;
 
         if (string.IsNullOrWhiteSpace(json))
