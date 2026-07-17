70bccc8fbacbaf41b318a2502d606bdcf507d9e2
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    small refactor - AspNetSpaTemplateExceptionJsonExtensionsJsonExtensions

--- Exceptions/AspNetSpaTemplateExceptionJsonExtensionsJsonExtensions.cs
+++ Exceptions/AspNetSpaTemplateExceptionJsonExtensionsJsonExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,77 +9,86 @@ namespace AspNetSpaTemplate.Exceptions;
 /// </summary>
 public static class AspNetSpaTemplateExceptionJsonExtensionsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        ReferenceHandler = ReferenceHandler.IgnoreCycles,
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+		ReferenceHandler = ReferenceHandler.IgnoreCycles,
+	};
 
-    private static readonly JsonSerializerOptions _jsonIndentedOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = true,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        ReferenceHandler = ReferenceHandler.IgnoreCycles,
-    };
+	/// <summary>
+	/// Serializes the <see cref="AspNetSpaTemplateException"/> to a JSON string.
+	/// </summary>
+	/// <param name="value">The exception to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>The JSON string representation of the exception.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <c>null</c>.</exception>
+	public static string ToJson(this AspNetSpaTemplateException value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-    /// <summary>
-    /// Serializes the <see cref="AspNetSpaTemplateException"/> to a JSON string.
-    /// </summary>
-    /// <param name="value">The exception to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the exception.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this AspNetSpaTemplateException value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+		return JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true } : _jsonOptions);
+	}
 
-        return JsonSerializer.Serialize(value, indented ? _jsonIndentedOptions : _jsonOptions);
-    }
+	/// <summary>
+	/// Deserializes a JSON string to an <see cref="AspNetSpaTemplateException"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>
+	/// The deserialized <see cref="AspNetSpaTemplateException"/> instance, or <c>null</c> if <paramref name="json"/> is <c>null</c>,
+	/// empty, or consists only of whitespace.
+	/// </returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is syntactically invalid.</exception>
+	public static AspNetSpaTemplateException? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-    /// <summary>
-    /// Deserializes a JSON string to an <see cref="AspNetSpaTemplateException"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized exception, or null if the JSON is invalid.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static AspNetSpaTemplateException? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-        try
-        {
-            return JsonSerializer.Deserialize<AspNetSpaTemplateException>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+		try
+		{
+			return JsonSerializer.Deserialize<AspNetSpaTemplateException>(json, _jsonOptions);
+		}
+		catch (JsonException)
+		{
+			return null;
+		}
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to an <see cref="AspNetSpaTemplateException"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized exception, or null if deserialization failed.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static bool TryFromJson(string json, out AspNetSpaTemplateException? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+	/// <summary>
+	/// Attempts to deserialize a JSON string to an <see cref="AspNetSpaTemplateException"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">
+	/// When this method returns <c>true</c>, contains the deserialized <see cref="AspNetSpaTemplateException"/>;
+	/// otherwise, <c>null</c>.
+	/// </param>
+	/// <returns><c>true</c> if deserialization succeeded; otherwise, <c>false</c>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
+	public static bool TryFromJson(string json, out AspNetSpaTemplateException? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            value = JsonSerializer.Deserialize<AspNetSpaTemplateException>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			value = null;
+			return false;
+		}
+
+		try
+		{
+			value = JsonSerializer.Deserialize<AspNetSpaTemplateException>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			value = null;
+			return false;
+		}
+	}
 }
