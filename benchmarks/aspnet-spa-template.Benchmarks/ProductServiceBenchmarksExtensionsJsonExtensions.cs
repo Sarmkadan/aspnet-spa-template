ad685a63adaaca3f1bd649d900e1c3caf871a432
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Readability pass over ProductServiceBenchmarksExtensionsJsonExtensions

--- benchmarks/aspnet-spa-template.Benchmarks/ProductServiceBenchmarksExtensionsJsonExtensions.cs
+++ benchmarks/aspnet-spa-template.Benchmarks/ProductServiceBenchmarksExtensionsJsonExtensions.cs
@@ -4,23 +4,25 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 /// <summary>
-/// Provides JSON serialization helpers for the <see cref="ProductServiceBenchmarksExtensions"/> class.
+/// Provides JSON serialization and deserialization extensions for the <see cref="ProductServiceBenchmarksExtensions"/> class.
 /// </summary>
 public static class ProductServiceBenchmarksExtensionsJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
     /// <summary>
-    /// Converts the <see cref="ProductServiceBenchmarksExtensions"/> instance to a JSON string.
+    /// Serializes the <see cref="ProductServiceBenchmarksExtensions"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The <see cref="ProductServiceBenchmarksExtensions"/> instance to convert.</param>
+    /// <param name="value">The <see cref="ProductServiceBenchmarksExtensions"/> instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON string with indentation.</param>
     /// <returns>A JSON string representation of the <see cref="ProductServiceBenchmarksExtensions"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this ProductServiceBenchmarksExtensions value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
-        var options = indented ? _jsonSerializerOptions with { WriteIndented = true } : _jsonSerializerOptions;
+        var options = indented
+            ? _jsonSerializerOptions with { WriteIndented = true }
+            : _jsonSerializerOptions;
         return JsonSerializer.Serialize(value, options);
     }
 
@@ -28,8 +30,8 @@ public static class ProductServiceBenchmarksExtensionsJsonExtensions
     /// Deserializes a JSON string to a <see cref="ProductServiceBenchmarksExtensions"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A <see cref="ProductServiceBenchmarksExtensions"/> instance, or null if the JSON string is invalid.</returns>
-    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    /// <returns>A <see cref="ProductServiceBenchmarksExtensions"/> instance, or <see langword="null"/> if the JSON string is invalid.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static ProductServiceBenchmarksExtensions? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -47,9 +49,9 @@ public static class ProductServiceBenchmarksExtensionsJsonExtensions
     /// Attempts to deserialize a JSON string to a <see cref="ProductServiceBenchmarksExtensions"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized <see cref="ProductServiceBenchmarksExtensions"/> instance, or null if the JSON string is invalid.</param>
+    /// <param name="value">The deserialized <see cref="ProductServiceBenchmarksExtensions"/> instance, or <see langword="null"/> if the JSON string is invalid.</param>
     /// <returns>True if the JSON string was successfully deserialized; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static bool TryFromJson(string json, out ProductServiceBenchmarksExtensions? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
