eb74bb8ea79ef1f64e03093ec10725085c550c9b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tidy up NotFoundExceptionExtensionsJsonExtensions

--- Exceptions/NotFoundExceptionExtensionsJsonExtensions.cs
+++ Exceptions/NotFoundExceptionExtensionsJsonExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -15,71 +15,68 @@ namespace AspNetSpaTemplate.Exceptions;
 /// </summary>
 public static class NotFoundExceptionExtensionsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    /// <summary>
-    /// Converts a <see cref="NotFoundException"/> to its JSON representation.
-    /// </summary>
-    /// <param name="value">The exception to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation.</param>
-    /// <returns>The JSON string representation of the exception.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this NotFoundException value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Converts a <see cref="NotFoundException"/> to its JSON representation.
+	/// </summary>
+	/// <param name="value">The exception to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation.</param>
+	/// <returns>The JSON string representation of the exception.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this NotFoundException value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        return JsonSerializer.Serialize(value, indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions);
-    }
+		return JsonSerializer.Serialize(value, indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions);
+	}
 
-    /// <summary>
-    /// Deserializes a <see cref="NotFoundException"/> from JSON.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static NotFoundException? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
+	/// <summary>
+	/// Deserializes a <see cref="NotFoundException"/> from JSON.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid.</exception>
+	public static NotFoundException? FromJson(string json)
+	{
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+		return JsonSerializer.Deserialize<NotFoundException>(json, _jsonOptions);
+	}
 
-        return JsonSerializer.Deserialize<NotFoundException>(json, _jsonOptions);
-    }
+	/// <summary>
+	/// Attempts to deserialize a <see cref="NotFoundException"/> from JSON.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized exception if successful.</param>
+	/// <returns>True if deserialization succeeded; otherwise, false.</returns>
+	public static bool TryFromJson(string json, out NotFoundException? value)
+	{
+		value = null;
 
-    /// <summary>
-    /// Attempts to deserialize a <see cref="NotFoundException"/> from JSON.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized exception if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out NotFoundException? value)
-    {
-        value = null;
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return true;
+		}
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return true;
-        }
-
-        try
-        {
-            value = JsonSerializer.Deserialize<NotFoundException>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		try
+		{
+			value = JsonSerializer.Deserialize<NotFoundException>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
