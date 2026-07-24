5e4afc8cc80e7cb24e156aef0090968373833801
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    EventBusImplementationTests changes
    should be backwards compatible.

--- tests/aspnet-spa-template.Tests/EventBusImplementationTests.cs
+++ tests/aspnet-spa-template.Tests/EventBusImplementationTests.cs
@@ -534,4 +534,283 @@ public class EventBusImplementationTests
         handler1Called.Should().BeFalse();
         handler2Called.Should().BeTrue();
     }
+
+    [Fact]
+    public async Task Unsubscribe_MidDispatch_ShouldNotThrowAndShouldNotAffectCurrentDispatch()
+    {
+        // Arrange
+        var executionOrder = new List<string>();
+        var unsubscribeDuringDispatch = new TaskCompletionSource<bool>();
+        var dispatchCompleted = new TaskCompletionSource<bool>();
+
+        Func<ProductCreatedEvent, Task> handler1 = _ => Task.Run(() =>
+        {
+            executionOrder.Add("handler1");
+            // Signal that we're in the middle of dispatch
+            unsubscribeDuringDispatch.SetResult(true);
+            // Wait a bit to ensure unsubscribe happens during dispatch
+            Thread.Sleep(50);
+        });
+
+        Func<ProductCreatedEvent, Task> handler2 = _ => Task.Run(() =>
+        {
+            executionOrder.Add("handler2");
+        });
+
+        Func<ProductCreatedEvent, Task> handler3 = _ => Task.Run(() =>
+        {
+            executionOrder.Add("handler3");
+            dispatchCompleted.SetResult(true);
+        });
+
+        _eventBus.Subscribe(handler1);
+        _eventBus.Subscribe(handler2);
+        _eventBus.Subscribe(handler3);
+
+        var @event = new ProductCreatedEvent
+        {
+            ProductId = 1,
+            ProductName = "Test Product",
+            Price = 9.99m
+        };
+
+        // Act - Start publishing and unsubscribe handler2 while dispatch is in progress
+        var publishTask = _eventBus.PublishAsync(@event);
+
+        // Wait until we're inside handler1
+        await unsubscribeDuringDispatch.Task.WaitAsync(TimeSpan.FromSeconds(1));
+
+        // Unsubscribe handler2 while dispatch is still in progress
+        // This should not throw and should not affect the current dispatch (handlers list is copied)
+        _eventBus.Unsubscribe(handler2);
+
+        // Wait for dispatch to complete
+        await dispatchCompleted.Task.WaitAsync(TimeSpan.FromSeconds(1));
+        await publishTask;
+
+        // Assert - All handlers should execute because PublishAsync copies the handlers list
+        executionOrder.Should().BeEquivalentTo(new[] { "handler1", "handler2", "handler3" });
+        // After dispatch completes, handler2 should be unsubscribed
+        _eventBus.GetSubscriberCount<ProductCreatedEvent>().Should().Be(2); // handler1 and handler3
+    }
+
+    [Fact]
+    public async Task PublishAsync_FromWithinHandler_ShouldNotDeadlockAndShouldExecuteAllHandlers()
+    {
+        // Arrange
+        var executionOrder = new List<string>();
+        var nestedPublishCompleted = new TaskCompletionSource<bool>();
+        var outerPublishCompleted = new TaskCompletionSource<bool>();
+
+        Func<ProductCreatedEvent, Task> handler1 = _ => Task.Run(() =>
+        {
+            executionOrder.Add("handler1-start");
+
+            // Publish another event from within handler (re-entrancy test)
+            var nestedEvent = new OrderPlacedEvent
+            {
+                OrderId = 1,
+                UserId = 1,
+                TotalAmount = 100.00m,
+                ItemCount = 5
+            };
+
+            // This should not deadlock or cause issues
+            _eventBus.PublishAsync(nestedEvent).GetAwaiter().GetResult();
+
+            executionOrder.Add("handler1-nested-publish-complete");
+            nestedPublishCompleted.SetResult(true);
+        });
+
+        Func<ProductCreatedEvent, Task> handler2 = _ => Task.Run(() =>
+        {
+            executionOrder.Add("handler2");
+            outerPublishCompleted.SetResult(true);
+        });
+
+        _eventBus.Subscribe(handler1);
+        _eventBus.Subscribe(handler2);
+
+        var @event = new ProductCreatedEvent
+        {
+            ProductId = 1,
+            ProductName = "Test Product",
+            Price = 9.99m
+        };
+
+        // Act - Publish event that will trigger nested publish
+        await _eventBus.PublishAsync(@event);
+
+        // Wait for nested publish to complete
+        await nestedPublishCompleted.Task.WaitAsync(TimeSpan.FromSeconds(2));
+        await outerPublishCompleted.Task.WaitAsync(TimeSpan.FromSeconds(2));
+
+        // Assert
+        executionOrder.Should().Contain("handler1-start");
+        executionOrder.Should().Contain("handler1-nested-publish-complete");
+        executionOrder.Should().Contain("handler2");
+        // Verify both handlers were called
+        _eventBus.GetSubscriberCount<ProductCreatedEvent>().Should().Be(2);
+    }
+
+    [Fact]
+    public async Task PublishAsync_FromWithinHandler_ShouldNotCauseInfiniteLoop()
+    {
+        // Arrange
+        var executionCount = 0;
+        var maxExecutionCount = 10; // Safety limit
+
+        Func<ProductCreatedEvent, Task> handler = _ => Task.Run(() =>
+        {
+            executionCount++;
+
+            // This handler publishes the same event type it handles
+            // Should not cause infinite loop due to copy of handlers list during dispatch
+            var sameEvent = new ProductCreatedEvent
+            {
+                ProductId = executionCount,
+                ProductName = "Test Product",
+                Price = (decimal)executionCount
+            };
+
+            // Try to publish from within handler
+            _eventBus.PublishAsync(sameEvent).GetAwaiter().GetResult();
+        });
+
+        _eventBus.Subscribe(handler);
+
+        var @event = new ProductCreatedEvent
+        {
+            ProductId = 1,
+            ProductName = "Test Product",
+            Price = 9.99m
+        };
+
+        // Act
+        await _eventBus.PublishAsync(@event);
+
+        // Assert - execution should be limited (not infinite)
+        executionCount.Should().BeLessThan(maxExecutionCount);
+    }
+
+    [Fact]
+    public async Task Unsubscribe_MultipleHandlersMidDispatch_ShouldNotAffectCurrentDispatch()
+    {
+        // Arrange
+        var executionOrder = new List<int>();
+        var unsubscribeSignal = new TaskCompletionSource<bool>();
+        var dispatchCompleted = new TaskCompletionSource<bool>();
+
+        Func<ProductCreatedEvent, Task> handler1 = _ => Task.Run(() =>
+        {
+            executionOrder.Add(1);
+            unsubscribeSignal.SetResult(true);
+            Thread.Sleep(50);
+        });
+
+        Func<ProductCreatedEvent, Task> handler2 = _ => Task.Run(() =>
+        {
+            executionOrder.Add(2);
+        });
+
+        Func<ProductCreatedEvent, Task> handler3 = _ => Task.Run(() =>
+        {
+            executionOrder.Add(3);
+        });
+
+        Func<ProductCreatedEvent, Task> handler4 = _ => Task.Run(() =>
+        {
+            executionOrder.Add(4);
+            dispatchCompleted.SetResult(true);
+        });
+
+        _eventBus.Subscribe(handler1);
+        _eventBus.Subscribe(handler2);
+        _eventBus.Subscribe(handler3);
+        _eventBus.Subscribe(handler4);
+
+        var @event = new ProductCreatedEvent
+        {
+            ProductId = 1,
+            ProductName = "Test Product",
+            Price = 9.99m
+        };
+
+        // Act - Start publishing and unsubscribe multiple handlers while dispatch is in progress
+        var publishTask = _eventBus.PublishAsync(@event);
+
+        // Wait until we're inside handler1
+        await unsubscribeSignal.Task.WaitAsync(TimeSpan.FromSeconds(1));
+
+        // Unsubscribe multiple handlers while dispatch is still in progress
+        // This should not throw and should not affect the current dispatch (handlers list is copied)
+        _eventBus.Unsubscribe(handler2);
+        _eventBus.Unsubscribe(handler3);
+
+        // Wait for dispatch to complete
+        await dispatchCompleted.Task.WaitAsync(TimeSpan.FromSeconds(1));
+        await publishTask;
+
+        // Assert - All handlers should execute because PublishAsync copies the handlers list
+        executionOrder.Should().BeEquivalentTo(new[] { 1, 2, 3, 4 });
+        // After dispatch completes, handlers 2 and 3 should be unsubscribed
+        _eventBus.GetSubscriberCount<ProductCreatedEvent>().Should().Be(2); // handler1 and handler4
+    }
+
+    [Fact]
+    public async Task ConcurrentUnsubscribe_ShouldNotCauseRaceConditions()
+    {
+        // Arrange
+        var handler1Called = false;
+        var handler2Called = false;
+        var handler3Called = false;
+        var completionSource = new TaskCompletionSource<bool>();
+
+        Func<ProductCreatedEvent, Task> handler1 = _ => Task.Run(() =>
+        {
+            handler1Called = true;
+        });
+
+        Func<ProductCreatedEvent, Task> handler2 = _ => Task.Run(() =>
+        {
+            handler2Called = true;
+            completionSource.SetResult(true);
+        });
+
+        Func<ProductCreatedEvent, Task> handler3 = _ => Task.Run(() =>
+        {
+            handler3Called = true;
+        });
+
+        _eventBus.Subscribe(handler1);
+        _eventBus.Subscribe(handler2);
+        _eventBus.Subscribe(handler3);
+
+        var @event = new ProductCreatedEvent
+        {
+            ProductId = 1,
+            ProductName = "Test Product",
+            Price = 9.99m
+        };
+
+        // Act - Publish event
+        await _eventBus.PublishAsync(@event);
+        await completionSource.Task.WaitAsync(TimeSpan.FromSeconds(1));
+
+        // Assert - All handlers should be called
+        handler1Called.Should().BeTrue();
+        handler2Called.Should().BeTrue();
+        handler3Called.Should().BeTrue();
+
+        // Now perform concurrent unsubscribes
+        var unsubscribeTasks = new Task[3];
+        unsubscribeTasks[0] = Task.Run(() => _eventBus.Unsubscribe(handler1));
+        unsubscribeTasks[1] = Task.Run(() => _eventBus.Unsubscribe(handler2));
+        unsubscribeTasks[2] = Task.Run(() => _eventBus.Unsubscribe(handler3));
+
+        await Task.WhenAll(unsubscribeTasks);
+
+        // Should not throw or cause race conditions
+        _eventBus.GetSubscriberCount<ProductCreatedEvent>().Should().Be(0);
+    }
 }
