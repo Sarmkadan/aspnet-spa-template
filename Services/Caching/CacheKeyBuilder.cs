556a445ead5ffc8600c77882b11af7559bfdafa8
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    Move cache key strings to Caching/CacheKeyBuilder.cs

---
+++ Services/Caching/CacheKeyBuilder.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+    public class CacheKeyBuilder
+    {
+        public Dictionary<string, string> GetCacheKeys()
+        {
+            var cacheKeys = new Dictionary<string, string>();
+            // TO DO: implement this method
+            return cacheKeys;
+        }
+    }
+}
