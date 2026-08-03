e7111476765ff6e410635853421925e2aed5aaaf
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-03 07:45:11 +0000

    chore: apply workspace updates

---
+++ benchmarks/aspnet-spa-template.Benchmarks/CacheMaintenanceWorkerBenchmarks.cs
@@ -0,0 +1,27 @@
+[MemoryDiagnoser]
+public class CacheMaintenanceWorkerBenchmarks
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+        // Setup test data here
+    }
+
+    [Benchmark]
+    public void Benchmark_Method1()
+    {
+        // Benchmark the first method here
+    }
+
+    [Benchmark]
+    public void Benchmark_Method2()
+    {
+        // Benchmark the second method here
+    }
+
+    [Benchmark]
+    public void Benchmark_Method3()
+    {
+        // Benchmark the third method here
+    }
+}
